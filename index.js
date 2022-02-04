d5c5b3f4b4c2a7cee9e43420a4b0403f2f12de22
Osmanys <ophernandez95@gmail.com>
2022-02-04 14:17:43 -0600

    added .env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+require('dotenv').config()
 const server = require('./api/server.js');
-const { PORT } = require('./config')
+const PORT = process.env.PORT
 
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
