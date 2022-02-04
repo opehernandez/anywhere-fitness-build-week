d63facea00f53e07cee9156e00d4fe940f15f87e
Osmanys <ophernandez95@gmail.com>
2022-02-04 14:29:45 -0600

    added production environment to .env

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,4 +1,5 @@
 // do not make changes to this file
+require('dotenv').config()
 const knex = require('knex');
 const knexConfig = require('../knexfile.js');
 const environment = process.env.NODE_ENV || 'development';
